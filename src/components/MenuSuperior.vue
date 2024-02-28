1ecc863fca944a4686d7a23b36d179b8a5eaafdf
luisc5martins <luisc.martinsjunior5@gmail.com>
2024-02-28 16:46:48 -0300

    menu e titulo do site

--- src/components/MenuSuperior.vue
+++ src/components/MenuSuperior.vue
@@ -11,7 +11,7 @@ const menuAberto = ref(false);
     <header>
       <div class="header--logo">
         <img src="@/assets/logo.png" alt="Logo" />
-        <h1>FakeStore</h1>
+        <h1>Not Store</h1>
       </div>
       <nav>
         <ul :class="menuAberto ? 'menu' : ''">
