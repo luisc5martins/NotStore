58711479e687a599492867890b9f3f1f4b4021e1
luisc5martins <luisc.martinsjunior5@gmail.com>
2024-03-20 16:53:16 -0300

    mudanças no button bar

--- src/components/FooterMobile.vue
+++ src/components/FooterMobile.vue
@@ -1,15 +1,34 @@
 <template>
-    
-    <footer>
-    <div>
-    <button>Politica de Privacidade</button>
-    <button>Termos Legais</button>
-    <button>Reemboloso</button>
+
+  <footer>
+    <div class="footer">
+      <div class="background-footer">
+        <button class="button">Home</button>
+        <button class="button">Carrinho</button>
+        <button class="button">Usuário</button>
+        <button class="button">Sobre</button>
+      </div>
     </div>
-    </footer>
-  
-  </template>
-  <style scoped>
-  .div {
-  }
+  </footer>
+
+</template>
+<style scoped>
+.footer {
+  text-align: center;
+  margin: 5px;
+}
+
+.button {
+  padding: 5px;
+  border-radius: 100px;
+  border-color: transparent;
+  background-color: white;
+  color: black;
+  font-size: 15px;
+}
+
+.background-footer {
+  background-color: white;
+  border-top: 1px black;
+}
 </style>
